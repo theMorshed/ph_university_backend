d7d5dc65038e3e35db77bccc1c14c8b4f0302132
theMorshed <themorshedctg@gmail.com>
2024-11-18 13:03:48 +0600

    add getallstudents getsinglestudent method and polish something

--- src/modules/student/student_controller.ts
+++ src/modules/student/student_controller.ts
@@ -16,6 +16,35 @@ const createStudent = async(req: Request, res: Response) => {
     }
 }
 
+const getAllStudents = async(req: Request, res: Response) => {
+    try {
+        const result = await StudentServices.getAllStudentsFromDB();
+        res.status(200).json({
+            success: true,
+            message: 'All students fetch successfully',
+            data: result
+        })
+    }catch(err) {
+        console.log(err);
+    }
+}
+
+const getSingleStudent = async(req: Request, res: Response) => {
+    try {
+        const {studentId} = req.params;
+        const result = await StudentServices.getSingleStudentFromDB(studentId);
+        res.status(200).json({
+            success: true,
+            message: `Student - ${studentId} retrieved successfully`,
+            data: result
+        })
+    }catch(err) {
+        console.log(err);
+    }
+}
+
 export const StudentControllers = {
     createStudent,
+    getAllStudents,
+    getSingleStudent
 }
--- src/modules/student/student_router.ts
+++ src/modules/student/student_router.ts
@@ -3,5 +3,7 @@ import { StudentControllers } from "./student_controller";
 
 const studentRouter = express.Router();
 studentRouter.post('/create-student', StudentControllers.createStudent);
+studentRouter.get('/', StudentControllers.getAllStudents);
+studentRouter.get('/:studentId', StudentControllers.getSingleStudent);
 
 export const StudentRoutes = studentRouter;
