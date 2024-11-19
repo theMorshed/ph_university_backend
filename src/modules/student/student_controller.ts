68672b5a70fe8b1b848ca6189bacdedc17681363
theMorshed <themorshedctg@gmail.com>
2024-11-19 08:27:29 +0600

    controller modified done

--- src/modules/student/student_controller.ts
+++ src/modules/student/student_controller.ts
@@ -12,7 +12,11 @@ const createStudent = async(req: Request, res: Response) => {
             data: result
         })
     } catch(err) {
-        console.log(err);
+        res.status(500).json({
+            success: false,
+            message: "Failed to retrieve data",
+            data: err,
+        })
     }
 }
 
@@ -25,7 +29,11 @@ const getAllStudents = async(req: Request, res: Response) => {
             data: result
         })
     }catch(err) {
-        console.log(err);
+        res.status(500).json({
+            success: false,
+            message: "Failed to retrieve data",
+            data: err,
+        })
     }
 }
 
@@ -39,7 +47,11 @@ const getSingleStudent = async(req: Request, res: Response) => {
             data: result
         })
     }catch(err) {
-        console.log(err);
+        res.status(500).json({
+            success: false,
+            message: "Failed to retrieve data",
+            data: err,
+        })
     }
 }
 
