77f1cb8c527875b788d434e1011947653206f5b4
theMorshed <themorshedctg@gmail.com>
2024-12-03 00:59:13 +0600

    academic semeter module created with his interface

---
+++ src/app/modules/academic_semester/academic_semester_interface.ts
@@ -0,0 +1,23 @@
+export type TAcademicSemesterName = 'Autumn' | 'Summer' | 'Fall';
+export type TAcademicSemesterCode = '01' | '02' | '03';
+type TMonth = 
+  | "January" 
+  | "February" 
+  | "March" 
+  | "April" 
+  | "May" 
+  | "June" 
+  | "July" 
+  | "August" 
+  | "September" 
+  | "October" 
+  | "November" 
+  | "December";
+
+export type TAcademicSemester = {
+    name: TAcademicSemesterName;
+    code: TAcademicSemesterCode;
+    year: string;
+    startMonth: TMonth;
+    endMonth: TMonth;
+}
